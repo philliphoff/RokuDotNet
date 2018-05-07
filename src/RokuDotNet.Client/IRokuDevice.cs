2caad0f2228a1ddf23c60625a4444efe081693be
Dana Hanna <dana.hanna@gmail.com>
2018-05-07 11:32:52 -0400

    added launch action

--- src/RokuDotNet.Client/IRokuDevice.cs
+++ src/RokuDotNet.Client/IRokuDevice.cs
@@ -1,15 +1,18 @@
 using System;
+using RokuDotNet.Client.Action;
 using RokuDotNet.Client.Input;
 using RokuDotNet.Client.Query;
 
 namespace RokuDotNet.Client
 {
-    public interface IRokuDevice : IRokuDeviceInput, IRokuDeviceQuery
+    public interface IRokuDevice 
     {
         string Id { get; }
 
         IRokuDeviceInput Input { get; }
 
         IRokuDeviceQuery Query { get; }
+
+        IRokuDeviceAction Action { get; }
     }
 }
