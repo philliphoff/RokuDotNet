3a4ca0cbc5b102e7525466f01b13147fca0db390
Phillip Hoff <phillip@orst.edu>
2018-12-09 16:54:23 -0800

    Sketch sample project.

---
+++ sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using RokuDotNet.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace RokuDotNet.Sample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var discoveryClient = new UdpRokuDeviceDiscoveryClient();
+
+            var device = await discoveryClient.DiscoverFirstDeviceAsync();
+
+            var result = await device.Query.GetAppsAsync();
+        }
+    }
+}
