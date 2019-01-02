5bc4423e1d98789fbd173147f791a99c476afc1b
Phillip Hoff <phillip@orst.edu>
2019-01-02 12:08:26 -0800

    Add sample project (#6)
    * Sketch sample project.
    
    * Run tests via VS Code.
    
    * Remove stray console write.

---
+++ sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using RokuDotNet.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace RokuDotNet.Sample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var discoveryClient = new UdpRokuDeviceDiscoveryClient();
+
+            var device = await discoveryClient.DiscoverFirstDeviceAsync();
+
+            var result = await device.Query.GetAppsAsync();
+        }
+    }
+}
