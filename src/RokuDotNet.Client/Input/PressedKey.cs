30c3b033f59af3c285478103bdd38c0538dabe75
Phillip Hoff <phillip@orst.edu>
2019-01-26 21:55:54 -0800

    Switch to struct.

--- src/RokuDotNet.Client/Input/PressedKey.cs
+++ src/RokuDotNet.Client/Input/PressedKey.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Diagnostics;
+
 namespace RokuDotNet.Client.Input
 {
-    public sealed class PressedKey : OneOf<SpecialKeys, char>
+    public struct PressedKey
     {
         public static implicit operator PressedKey(SpecialKeys key)
         {
@@ -12,12 +15,53 @@ namespace RokuDotNet.Client.Input
             return new PressedKey(key);
         }
 
-        public PressedKey(SpecialKeys key) : base(key)
+        private readonly char? literalKey;
+        private readonly SpecialKeys? specialKey;
+
+        public PressedKey(SpecialKeys key)
         {
+            this.specialKey = key;
+            this.literalKey = null;
         }
 
-        public PressedKey(char key) : base(key)
+        public PressedKey(char key)
         {
+            this.specialKey = null;
+            this.literalKey = key;
+        }
+
+        public void Match(Action<SpecialKeys> onSpecialKey, Action<char> onLiteralKey)
+        {
+            if (specialKey.HasValue)
+            {
+                onSpecialKey(this.specialKey.Value);
+            }
+            else if (literalKey.HasValue)
+            {
+                onLiteralKey(this.literalKey.Value);
+            }
+            else
+            {
+                Debug.Fail("One of SpecialKeys or char should always have a value.");
+            }
+        }
+
+        public TResult Match<TResult>(Func<SpecialKeys, TResult> onT1, Func<char, TResult> onT2)
+        {
+            if (specialKey.HasValue)
+            {
+                return onT1(this.specialKey.Value);
+            }
+            else if (literalKey.HasValue)
+            {
+                return onT2(this.literalKey.Value);
+            }
+            else
+            {
+                Debug.Fail("One of SpecialKeys or char should always have a value.");
+
+                return default(TResult);
+            }
         }
     }
 }
